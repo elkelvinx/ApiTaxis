b500f2d2fd4fcb9294c5b2c98e67baa74420a72b
Antonio <elkelvin484@gmail.com>
2024-08-25 19:22:44 -0700

    modificaciones para terminar los LOGS de historyLogIn y changeLog

---
+++ Entidades/Logs/historyLogIn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Entidades.Logs
+{
+    public class historyLogIn
+    {
+        [Serializable]
+        [DataContract]
+        public class regtisterLogIn
+        {
+            [DataMember]
+            public int id { get; set; }
+            [DataMember]
+            public int idUser { get; set; }
+            [DataMember]
+            public string userName { get; set; }
+            [DataMember]
+            public int idRole { get; set; }
+            [DataMember]
+            public DateTime entry { get; set; }
+            [DataMember]
+            public DateTime exits { get; set; }
+        }
+        public class listHistoryLog : List<historyLogIn> { }
+    }
+}
---
+++ Servicios/Logs/ServicioChangeLog.cs
@@ -0,0 +1,63 @@
+﻿using Entidades.Logs;
+using System;
+using System.Data.SqlClient;
+using System.Security.Claims;
+using System.Threading;
+
+namespace Servicios.Logs
+{
+    public class ServicioChangeLog
+    {
+        public static string UpdateTriggerChangeLog(string nameTable, int DML, SqlCommand query)
+        {
+            var obj = new ChangeLog();
+            var response = "ok";
+            string cadena = "insert into changeLog (userName,idUser,roleName,nameTable,modDate,DML,query) Values (@userName,@idUser,@roleName," +
+                "@nameTable,@modDate,@DML,@query)";
+            //Obtencion de los claims necesarios
+            ClaimsPrincipal claimsPrincipal = Thread.CurrentPrincipal as ClaimsPrincipal;
+            if (claimsPrincipal != null)
+            {
+                obj.UserName = claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier).Value;
+                obj.roleName = claimsPrincipal.FindFirst(ClaimTypes.Role).Value;
+                obj.idUser = Int16.Parse(claimsPrincipal.FindFirst("idUser").Value);
+            }
+            //Obtencion query ejecutado y formateo
+            string queryCompleto = query.CommandText;
+            foreach (SqlParameter param in query.Parameters)
+            {
+                try
+                {
+                    queryCompleto = queryCompleto.Replace(param.ParameterName, "\"" + param.Value.ToString() + "\"");
+                }
+                catch (Exception) 
+                {
+                    queryCompleto = queryCompleto.Replace(param.ParameterName, "Error");
+                    DML = 999; //SI DML = 999 hay un error
+                }
+            }
+            using (SqlConnection con = ServiciosBD.ObtenerConexion())
+            {
+                using (SqlCommand cmd = new SqlCommand(cadena, con))
+                {
+                    cmd.Parameters.AddWithValue("@userName", obj.UserName);
+                    cmd.Parameters.AddWithValue("@idUser", obj.idUser);
+                    cmd.Parameters.AddWithValue("@roleName", obj.roleName);
+                    cmd.Parameters.AddWithValue("@nameTable", nameTable);
+                    cmd.Parameters.AddWithValue("@modDate", DateTime.UtcNow);
+                    cmd.Parameters.AddWithValue("@DML", DML);
+                    cmd.Parameters.AddWithValue("@query", queryCompleto);
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                    catch (Exception e) { return response = e.Message; }
+
+                    return response;
+                }
+
+            }
+        }
+    }
+}
