93ad22d18a45332a7cab9cb1bbd9e609d1b918e4
Antonio <ibarra.mondaca343@gmail.com>
2025-08-21 00:51:34 -0700

    the home controller working ready to show the graphics in home

--- Entidades/Home.cs
+++ Entidades/Home.cs
@@ -1,27 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Entidades
 {
-    [Serializable]
+    // Rango de fechas(para endpoints de gráficas)
     [DataContract]
-    public class HomeDriversRise
-    {
-        [DataMember]
-        public int year { get; set; }
-        [DataMember]
-        public int month { get; set; }
-        [DataMember]
-        public int totalDrivers { get; set; }
-    }
-    public class listHomeDriversRise : List<HomeDriversRise> { }
     public class DateRangeDto
     {
-        public DateTime? StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
+        [DataMember] public DateTime? StartDate { get; set; }
+        [DataMember] public DateTime? EndDate { get; set; }
+    }
+
+    // Punto mensual genérico (para series)
+    [Serializable]
+    [DataContract]
+    public class MonthPoint
+    {
+        [DataMember] public int Year { get; set; }
+        [DataMember] public int Month { get; set; }
+        [DataMember] public int Total { get; set; }
+    }
+    public class MonthPointList : List<MonthPoint> { }
+
+
+    [Serializable]
+    [DataContract]
+    public class CountMonthly
+    {
+        [DataMember] public int Current { get; set; }
+        [DataMember] public int Previous { get; set; }
+    }
+    public class KpiData
+    {
+        [DataMember] public int CurrentMonth { get; set; }
+        [DataMember] public int PreviousMonth { get; set; }
+        [DataMember] public double Percentage { get; set; } 
     }
 }
